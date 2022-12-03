2b6d0b97c69e5363bb6d0b83092fb7bd96868bc5
Tommy Duchesne <tduchesne@dimonoff.com>
2022-12-03 01:38:07 -0500

    fix template

--- 00/main1.py
+++ 00/main1.py
@@ -1,16 +1,11 @@
 import os
 
 
-INPUT_FILE = "input1"
+INPUT_FILE = "input_test"
 
 with open(f"{os.path.dirname(__file__)}/{INPUT_FILE}", mode='r') as file:
     input_ = [line.strip() for line in file.readlines() if line]
 
 
-def solve():
-    for line in input_:
-        print(line)
-
-
-if __name__ == '__main__':
-    solve()
+for line in input_:
+    print(line)
