acdaf64c1b154042c7afe0ea7b96772bf0b45b16
Tommy Duchesne <tduchesne@dimonoff.com>
2022-12-04 01:13:45 -0500

    fixed by not using sets

--- 04/main2.py
+++ 04/main2.py
@@ -14,9 +14,16 @@ def read_line(line: str) -> tuple[tuple[int, int], tuple[int, int]]:
 
 
 def pairs_are_overlapping(pairs: tuple[tuple[int, int], tuple[int, int]]) -> bool:
-    set0 = set(range(pairs[0][0], pairs[0][1]+1))
-    set1 = set(range(pairs[1][0], pairs[1][1]+1))
-    return len(set0 & set1) > 0
+    g0, g1 = pairs
+
+    def in_range(n: int, g: tuple[int, int]) -> bool:
+        return g[0] <= n <= g[1]
+
+    if in_range(g0[0], g1) or in_range(g0[1], g1):
+        return True
+    if in_range(g1[0], g0) or in_range(g1[1], g0):
+        return True
+    return False
 
 
 parsed_groups = list(map(read_line, input_))
