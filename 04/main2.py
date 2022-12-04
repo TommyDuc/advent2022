fed4fe857c97082ade720339061074bdd917f353
Tommy Duchesne <tduchesne@dimonoff.com>
2022-12-04 00:52:12 -0500

    day 4

---
+++ 04/main2.py
@@ -0,0 +1,24 @@
+import os
+
+INPUT_FILE = "input"
+
+with open(f"{os.path.dirname(__file__)}/{INPUT_FILE}", mode='r') as file:
+    input_ = [l for line in file.readlines() if (l := line.strip('\n '))]
+
+
+def read_line(line: str) -> tuple[tuple[int, int], tuple[int, int]]:
+    pairs = line.split(',')
+    range_str1 = pairs[0].split('-')
+    range_str2 = pairs[1].split('-')
+    return (int(range_str1[0]), int(range_str1[1])), (int(range_str2[0]), int(range_str2[1]))
+
+
+def pairs_are_overlapping(pairs: tuple[tuple[int, int], tuple[int, int]]) -> bool:
+    set0 = set(range(pairs[0][0], pairs[0][1]+1))
+    set1 = set(range(pairs[1][0], pairs[1][1]+1))
+    return len(set0 & set1) > 0
+
+
+parsed_groups = list(map(read_line, input_))
+contained_groups = list(filter(pairs_are_overlapping, parsed_groups))
+print(len(contained_groups))
